fe53301d3fe930833d4fe643454db3b624943313
Byunghoon Hwang <52238766+firstdeep@users.noreply.github.com>
2023-02-03 21:08:32 +0900

    [Gold III] Title: LCA, Time: 40 ms, Memory: 16696 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -1,44 +1,45 @@
-#if 1
+#if 1 
 #include<cstdio>
-#include<cmath>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 #include<iostream>
-
 using namespace std;
-int N, M, MAXk;
+int N, M,MAXK;
+bool visit[50001];
 vector<int> ad[100001];
-bool visit[100001];
-int parent[100001][21], depth[100001]; // 부모가 누구인지를 저장하는 변수와 depth를 저장하는 변수 
+int parent[100001][21], depth[100001];
 
-void DFS(int curr, int depth_value) {
+
+void DFS(int curr, int d) {
 	visit[curr] = true;
-	depth[curr] = depth_value;
+	depth[curr] = d;
 	for (auto next : ad[curr]) {
 		if (!visit[next]) {
 			parent[next][0] = curr;
-			DFS(next, depth_value + 1);
+			DFS(next, d + 1);
 		}
 	}
 }
-
 void fillparent() {
-	for (int k = 1; k <= MAXk; k++) {
+	for (int k = 1; k <= MAXK; k++) {
 		for (int i = 1; i <= N; i++) {
 			parent[i][k] = parent[parent[i][k - 1]][k - 1];
 		}
 	}
 }
 
+
 int LCA(int a, int b) {
 	if (depth[b] < depth[a]) swap(a, b);
-	for (int k = MAXk; k >= 0; k--) {
+	for (int k = MAXK; k >= 0; k--) {
 		int diff = depth[b] - depth[a];
-		if (diff >= (1 << k)) b = parent[b][k]; // 부모노드로 할당한다.
+		if (diff >= (1 << k))b = parent[b][k];
 	}
-	if (b == a) return b;
 
-	for (int k = MAXk; k >= 0; k--) {
+	if (a == b) return b;
+
+	for (int k = MAXK; k >=0; k--) {
 		if (parent[b][k] != parent[a][k]) {
 			b = parent[b][k];
 			a = parent[a][k];
@@ -46,6 +47,8 @@ int LCA(int a, int b) {
 	}
 	return parent[b][0];
 }
+
+
 int main() {
 	scanf("%d", &N);
 	for (int i = 0; i < N - 1; i++) {
@@ -55,9 +58,10 @@ int main() {
 		ad[v].push_back(u);
 	}
 	DFS(1, 0);
-	MAXk = (int)floor(log2(N)); // N까지만 사용하면 최대 높이를 알 수 있다. 
+	MAXK = (int)floor(log2(N)); // N까지만 사용하면 최대 높이를 알 수 있다. 
 	fillparent();
 	
+
 	scanf("%d", &M);
 	while (M--) {
 		int a, b;
